2c3d8b71142717035fc256aac88f4fbc147fc527
buritbest321 <burit.sih@gmail.com>
2024-10-24 19:20:52 +0700

    Restructure

--- Java/lab01.py
+++
@@ -1,8 +0,0 @@
-# Fahrenheit to Kevin 
-user_F = float(input("Enter your Fahrenheit: "))
-K = (user_F + 459.67) / 5 / 9; 
-print("Kelvin is now: %.2f" % (K))
-
-# Kelvin to Fahrenheit
-F = (9 / 5 * K) - 459.67
-print("Fahrenheit is now: %.2f" % (F))
